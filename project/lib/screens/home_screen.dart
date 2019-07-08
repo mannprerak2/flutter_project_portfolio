3f259400f48f5e668101bd95e388f88dec041e9b
Prerak Mann <mannprerak2@gmail.com>
2019-07-08 15:05:25 +0530

    added scrolling with up/down and space, added build.yaml for optimisation

--- project/lib/screens/home_screen.dart
+++ project/lib/screens/home_screen.dart
@@ -6,39 +6,68 @@ import 'package:portfolio/widgets/project_ui.dart';
 import 'package:portfolio/widgets/responsive_widget.dart';
 import 'package:portfolio/flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 
-class HomePage extends StatelessWidget {
+import 'dart:html' as html;
+
+class HomePage extends StatefulWidget {
   static final scaffoldKey = GlobalKey<ScaffoldState>();
   static final controller = ScrollController();
+
+  @override
+  _HomePageState createState() => _HomePageState();
+
+  static void scrollTo(double offset, {bool absolute = false}) {
+    double pos = offset;
+
+    if (!absolute) {
+      pos += HomePage.controller.offset;
+    }
+
+    HomePage.controller.animateTo(
+      pos,
+      duration: Duration(milliseconds: absolute ? 1000 : 200),
+      curve: Curves.decelerate,
+    );
+  }
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    html.window.onKeyDown.listen((e) {
+      if (e.keyCode == html.KeyCode.DOWN || e.keyCode == html.KeyCode.SPACE) {
+        HomePage.scrollTo(150);
+      } else if (e.keyCode == html.KeyCode.UP) {
+        HomePage.scrollTo(-150);
+      }
+    });
+    super.initState();
+  }
+
   final menuChildren = <Widget>[
     MenuButton(
       text: "About Me",
       onPressed: () {
-        if (scaffoldKey.currentState.isDrawerOpen) {
-          Navigator.of(scaffoldKey.currentContext).pop();
+        if (HomePage.scaffoldKey.currentState.isDrawerOpen) {
+          Navigator.of(HomePage.scaffoldKey.currentContext).pop();
         }
-        controller.animateTo(
-          0,
-          duration: Duration(seconds: 1),
-          curve: Curves.decelerate,
-        );
+        HomePage.scrollTo(0, absolute: true);
       },
     ),
     MenuButton(
       text: "Projects",
       onPressed: () {
-        if (scaffoldKey.currentState.isDrawerOpen) {
-          Navigator.of(scaffoldKey.currentContext).pop();
+        if (HomePage.scaffoldKey.currentState.isDrawerOpen) {
+          Navigator.of(HomePage.scaffoldKey.currentContext).pop();
         }
         //TODO: change this whenever layout is updated in height
-        controller.animateTo(
-          ResponsiveWidget.isLargeScreen(scaffoldKey.currentContext)
-              ? 350
-              : ResponsiveWidget.isMediumScreen(scaffoldKey.currentContext)
-                  ? 310
-                  : 630,
-          duration: Duration(seconds: 1),
-          curve: Curves.decelerate,
-        );
+        HomePage.scrollTo(
+            ResponsiveWidget.isLargeScreen(HomePage.scaffoldKey.currentContext)
+                ? 350
+                : ResponsiveWidget.isMediumScreen(
+                        HomePage.scaffoldKey.currentContext)
+                    ? 310
+                    : 630,
+            absolute: true);
       },
     ),
   ];
@@ -49,7 +78,7 @@ class HomePage extends StatelessWidget {
         ? 3
         : ResponsiveWidget.isSmallScreen(context) ? 1 : 2;
     return Scaffold(
-      key: scaffoldKey,
+      key: HomePage.scaffoldKey,
       drawer: ResponsiveWidget.isSmallScreen(context)
           ? Drawer(
               child:
@@ -57,7 +86,7 @@ class HomePage extends StatelessWidget {
             )
           : null,
       body: CustomScrollView(
-        controller: controller,
+        controller: HomePage.controller,
         slivers: <Widget>[
           SliverAppBar(
             elevation: 10,
