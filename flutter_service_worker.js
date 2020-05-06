85bb30eb1f9c21a4897087c615a5ba48651299d8
bot <bot>
2020-05-06 10:20:13 +0000

    auto gh-action, build web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,14 +1,15 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/assets/AssetManifest.json": "28b402bf75261f7815002cbaa66654b5",
-"/assets/LICENSE": "a381ea3ca61da09a52e9376625df0840",
-"/assets/assets/GoogleSansRegular.ttf": "b5c77a6aed75cdad9489effd0d5ea411",
-"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
-"/assets/FontManifest.json": "08792490555b179235543cfffa3ed5ec",
-"/main.dart.js": "b0749e0a6623d36f9576d43bbc392c6d",
-"/index.html": "f99404f7a8943b385e6c661aaecc18e4"
+  "index.html": "f99404f7a8943b385e6c661aaecc18e4",
+"/": "f99404f7a8943b385e6c661aaecc18e4",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/assets/GoogleSansRegular.ttf": "b5c77a6aed75cdad9489effd0d5ea411",
+"assets/FontManifest.json": "08792490555b179235543cfffa3ed5ec",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"assets/AssetManifest.json": "28b402bf75261f7815002cbaa66654b5",
+"assets/LICENSE": "b4e03532f23aa16099d4dd9e54970f8f",
+"main.dart.js": "373e4c16f9494e9903857fb60bed5081"
 };
 
 self.addEventListener('activate', function (event) {
